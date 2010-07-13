bf05bb45731d9ecfcd632df2ac89624697f56715
Carlos Cavieres T <ultimute@gmail.com>
2010-07-13 13:10:04 -0400

    había un bug en que no se mostraban algunas caras del cubo

--- cubo.cpp
+++ cubo.cpp
@@ -22,9 +22,9 @@ void Cubo::draw(float deltaTime){
             // Cara de Atras
             glNormal3f( 0.0f, 0.0f, -1.0f);      //defino la normal...
             glVertex3f( 1.0f, -1.0f, -1.0f);
-            glVertex3f( 1.0f,  1.0f, -1.0f);
-            glVertex3f(-1.0f,  1.0f, -1.0f);
             glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f,  1.0f, -1.0f);
+            glVertex3f( 1.0f,  1.0f, -1.0f);
 
             // Cara de Adelante
             glNormal3f( 0.0f, 0.0f, 1.0f);      //defino la normal...
@@ -36,23 +36,23 @@ void Cubo::draw(float deltaTime){
             // Right face
             glNormal3f( 1.0f, 0.0f, 0.0f);
             glVertex3f( 1.0f, -1.0f,  1.0f);
-            glVertex3f( 1.0f,  1.0f,  1.0f);
-            glVertex3f( 1.0f,  1.0f, -1.0f);
             glVertex3f( 1.0f, -1.0f, -1.0f);
+            glVertex3f( 1.0f,  1.0f, -1.0f);
+            glVertex3f( 1.0f,  1.0f,  1.0f);
 
             // Left Face
             glNormal3f(-1.0f, 0.0f, 0.0f);
-            glVertex3f(-1.0f,  1.0f, -1.0f);
             glVertex3f(-1.0f,  1.0f,  1.0f);
-            glVertex3f(-1.0f, -1.0f,  1.0f);
+            glVertex3f(-1.0f,  1.0f, -1.0f);
             glVertex3f(-1.0f, -1.0f, -1.0f);
+            glVertex3f(-1.0f, -1.0f,  1.0f);
 
             // Top Face
             glNormal3f( 0.0f, 1.0f, 0.0f);
             glVertex3f( 1.0f,  1.0f, -1.0f);
-            glVertex3f( 1.0f,  1.0f,  1.0f);
-            glVertex3f(-1.0f,  1.0f,  1.0f);
             glVertex3f(-1.0f,  1.0f, -1.0f);
+            glVertex3f(-1.0f,  1.0f,  1.0f);
+            glVertex3f( 1.0f,  1.0f,  1.0f);
 
             // Bottom Face
             glNormal3f( 0.0f, -1.0f, 0.0f);
